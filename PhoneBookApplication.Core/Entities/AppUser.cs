dcaefc81fb35e4f70c2717d67d778cd7c9e1e53f
rocklessg <abdulhafiz.suleiman@decagon.dev>
2022-04-23 09:01:12 +0100

    Entities layer set

---
+++ PhoneBookApplication.Core/Entities/AppUser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBookApplication.Core.Entities
+{
+    public class AppUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+
+        //Establishing Relationship between the phonebook(user) and the entries(contacts)
+        public ICollection<Contact> Contacts { get; set; }
+    }
+}
